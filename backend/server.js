dbc884e44d0917de0fa5b5086ab9c0cf691beeaf
G. VAISHNAVI <vaishnavigk41@gmail.com>
2026-02-05 23:28:48 +0530

    Refactor server.js to improve route variable names
    Signed-off-by: G. VAISHNAVI <vaishnavigk41@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
-const complaintRoutes = require("./routes/complaints");
-const paymentRoutes = require("./routes/payments");
+const complaintsRoutes = require("./routes/complaints");
+const paymentsRoutes = require("./routes/payments");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/complaints", complaintRoutes);
-app.use("/api/payments", paymentRoutes);
-
-app.get("/", (req, res) => {
-  res.send("SUVIDHA Backend Running");
-});
+app.use("/api/complaints", complaintsRoutes);
+app.use("/api/payments", paymentsRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`SUVIDHA Backend running on port ${PORT}`);
 });
